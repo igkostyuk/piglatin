18a0fdd08530cea0dc37db73100408735d65a6e6
Igor kostyuk <ig.fire@gmail.com>
2020-12-13 10:43:50 +0200

    refactor: piglatin after linter

--- piglatin.go
+++ piglatin.go
@@ -11,7 +11,7 @@ const (
 )
 
 func translateWord(word string) string {
-	if len(word) == 0 {
+	if word == "" {
 		return word
 	}
 	capitalazed := unicode.IsUpper(rune(word[0]))
@@ -19,7 +19,7 @@ func translateWord(word string) string {
 
 	switch {
 	case firstVowelIndex == 0:
-		word = word + vowelSuffix
+		word += vowelSuffix
 	case firstVowelIndex > 0:
 		consonant := word[:firstVowelIndex]
 		endOfWord := word[firstVowelIndex:]
@@ -30,23 +30,26 @@ func translateWord(word string) string {
 	if capitalazed {
 		word = strings.Title(word)
 	}
+
 	return word
 }
 
-//Translate translate english phrace or word into the piglatina
+// Translate translate english phrace or word into the piglatina.
 func Translate(phrase string) string {
 	words := strings.Fields(phrase)
 	for i, w := range words {
 		w = strings.TrimRightFunc(w, isNotLetter)
 		words[i] = strings.Replace(words[i], w, translateWord(w), 1)
 	}
+
 	return strings.Join(words, " ")
 }
 
 func lower(r rune) rune {
-	if 'A' <= r && r <= 'Z' {
+	if r >= 'A' && r <= 'Z' {
 		r += 'a' - 'A'
 	}
+
 	return r
 }
 
