63ea7ee3bc85d702862b775da2bbd7960b54e48f
Igor kostyuk <ig.fire@gmail.com>
2020-12-13 08:47:22 +0200

    docs: add doc for package

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package for translating from English to Pig Latin.
+//
+// Usage
+//
+//  w := "Hello World!"
+//  pw := piglatin.Translate(e)
+//  log.Printf("%s is %s in pig latin.", w, pw)
+//
+package piglatin
